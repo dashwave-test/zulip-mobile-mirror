bc2536ffa1ceacbcfc650c5125154302d06ffe43
Dashwave <tech@dashwave.io>
2024-09-25 20:19:02 +0000

    updated profile screen

--- src/account-info/ProfileScreen.js
+++ src/account-info/ProfileScreen.js
@@ -1,4 +1,3 @@
-/* @flow strict-local */
 import React, { useContext } from 'react';
 import type { Node } from 'react';
 import { ScrollView, View } from 'react-native';
@@ -11,7 +10,6 @@ import type { RouteProp } from '../react-navigation';
 import type { MainTabsNavigationProp } from '../main/MainTabsScreen';
 import { createStyleSheet } from '../styles';
 import { useDispatch, useSelector } from '../react-redux';
-import ZulipButton from '../common/ZulipButton';
 import { logout } from '../account/logoutActions';
 import { tryStopNotifications } from '../notification/notifTokens';
 import AccountDetails from './AccountDetails';
@@ -26,6 +24,7 @@ import SwitchRow from '../common/SwitchRow';
 import * as api from '../api';
 import { identityOfAccount } from '../account/accountMisc';
 import NavRow from '../common/NavRow';
+import ZulipText from '../common/ZulipText';
 import { emojiTypeFromReactionType } from '../emoji/data';
 
 const styles = createStyleSheet({
@@ -33,94 +32,17 @@ const styles = createStyleSheet({
     flexDirection: 'row',
     marginHorizontal: 8,
   },
-  button: {
-    flex: 1,
-    margin: 8,
-  },
 });
 
-function ProfileButton(props: {| +ownUserId: UserId |}) {
+function ProfileScreen(props: {|
+  navigation: MainTabsNavigationProp<'profile'>,
+  route: RouteProp<'profile', void>,
+|}): Node {
   const navigation = useNavigation();
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Full profile"
-      onPress={() => {
-        navigation.push('account-details', { userId: props.ownUserId });
-      }}
-    />
-  );
-}
-
-function SettingsButton(props: {||}) {
-  const navigation = useNavigation();
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Settings"
-      onPress={() => {
-        navigation.push('settings');
-      }}
-    />
-  );
-}
-
-function SwitchAccountButton(props: {||}) {
-  const navigation = useNavigation();
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Switch account"
-      onPress={() => {
-        navigation.push('account-pick');
-      }}
-    />
-  );
-}
-
-function LogoutButton(props: {||}) {
   const dispatch = useDispatch();
   const _ = useContext(TranslationContext);
   const account = useSelector(getAccount);
   const identity = identityOfAccount(account);
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Log out"
-      onPress={() => {
-        showConfirmationDialog({
-          destructive: true,
-          title: 'Log out',
-          message: {
-            text: 'This will log out {email} on {realmUrl} from the mobile app on this device.',
-            values: { email: identity.email, realmUrl: identity.realm.toString() },
-          },
-          onPressConfirm: () => {
-            dispatch(tryStopNotifications(account));
-            dispatch(logout());
-          },
-          _,
-        });
-      }}
-    />
-  );
-}
-
-type Props = $ReadOnly<{|
-  navigation: MainTabsNavigationProp<'profile'>,
-  route: RouteProp<'profile', void>,
-|}>;
-
-/**
- * The profile/settings/account screen we offer among the main tabs of the app.
- */
-export default function ProfileScreen(props: Props): Node {
-  const navigation = useNavigation();
-
   const auth = useSelector(getAuth);
   const zulipFeatureLevel = useSelector(getZulipFeatureLevel);
   const ownUser = useSelector(getOwnUser);
@@ -128,7 +50,6 @@ export default function ProfileScreen(props: Props): Node {
   const presenceEnabled = useSelector(state => getRealm(state).presenceEnabled);
   const awayStatus = useSelector(state => getUserStatus(state, ownUserId).away);
   const userStatus = useSelector(state => getUserStatus(state, ownUserId));
-
   const { status_emoji, status_text } = userStatus;
 
   return (
@@ -155,15 +76,12 @@ export default function ProfileScreen(props: Props): Node {
         {zulipFeatureLevel >= 148 ? (
           <SwitchRow
             label="Invisible mode"
-            /* $FlowIgnore[incompatible-cast] - Only null when FL is <89;
-               see comment on RealmState['presenceEnabled'] */
             value={!(presenceEnabled: boolean)}
             onValueChange={(newValue: boolean) => {
               api.updateUserSettings(auth, { presence_enabled: !newValue }, zulipFeatureLevel);
             }}
           />
         ) : (
-          // TODO(server-6.0): Remove.
           <SwitchRow
             label="Set yourself to away"
             value={awayStatus}
@@ -172,17 +90,46 @@ export default function ProfileScreen(props: Props): Node {
             }}
           />
         )}
-        <View style={styles.buttonRow}>
-          <ProfileButton ownUserId={ownUser.user_id} />
-        </View>
-        <View style={styles.buttonRow}>
-          <SettingsButton />
-        </View>
-        <View style={styles.buttonRow}>
-          <SwitchAccountButton />
-          <LogoutButton />
-        </View>
+        <NavRow
+          title="Full profile"
+          onPress={() => {
+            navigation.push('account-details', { userId: ownUserId });
+          }}
+        />
+        <NavRow
+          title="Settings"
+          onPress={() => {
+            navigation.push('settings');
+          }}
+        />
+        <NavRow
+          title="Switch account"
+          onPress={() => {
+            navigation.push('account-pick');
+          }}
+        />
+        <NavRow
+          title="Log out"
+          onPress={() => {
+            showConfirmationDialog({
+              destructive: true,
+              title: 'Log out',
+              message: {
+                text: 'This will log out {email} on {realmUrl} from the mobile app on this device.',
+                values: { email: identity.email, realmUrl: identity.realm.toString() },
+              },
+              onPressConfirm: () => {
+                dispatch(tryStopNotifications(account));
+                dispatch(logout());
+              },
+              _,
+            });
+          }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+export default ProfileScreen;
+

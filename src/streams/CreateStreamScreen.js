a88cfa07f6c8a9b6f4c64d0eff1ade5b39f18987
Nupur Agrawal <nupur202000@gmail.com>
2024-09-30 05:25:55 +0000

    Add feedback when attempting to make a default stream private

--- src/streams/CreateStreamScreen.js
+++ src/streams/CreateStreamScreen.js
@@ -29,9 +29,6 @@ export default function CreateStreamScreen(props: Props): Node {
 
   const handleComplete = useCallback(
     async ({ name, description, privacy }) => {
-      // This will miss existing streams that the client can't know about;
-      // for example, a private stream the user can't access. See comment
-      // where we catch an `ApiError`, below.
       if (streamsByName.has(name)) {
         showErrorAlert(_('A stream with this name already exists.'));
         return false;
@@ -41,16 +38,6 @@ export default function CreateStreamScreen(props: Props): Node {
         await api.createStream(auth, { name, description, ...privacyToStreamProps(privacy) });
         return true;
       } catch (error) {
-        // If the stream already exists but you can't access it (e.g., it's
-        // private), then it won't be in the client's data structures, so
-        // our client-side check above won't stop the request from being
-        // made. In that case, we expect the server to always give an error,
-        // because you can't subscribe to a stream that you can't access.
-        // That error will have:
-        // - error.message: `Unable to access stream (${streamName})`, or a
-        //   translation of that into the user's own language
-        // - error.code: "BAD_REQUEST" (as of server feature level 126;
-        //   possibly the server should be more specific)
         if (error instanceof ApiError) {
           showErrorAlert(error.message);
           return false;

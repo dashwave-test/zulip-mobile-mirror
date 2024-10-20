7ecb7e04597881ad35031775c2f743f1d13a4625
Dashwave <tech@dashwave.io>
2024-10-20 13:44:30 +0000

    Fix spacing in ModalNavBar to reduce empty space

--- src/nav/ModalNavBar.js
+++ src/nav/ModalNavBar.js
@@ -1,7 +1,7 @@
 /* @flow strict-local */
 import React, { useContext, useMemo } from 'react';
 import type { Node } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, View } from 'react-native-safe-area-context';
 
 import type { LocalizableReactText } from '../types';
 import globalStyles, { ThemeContext, NAVBAR_SIZE } from '../styles';
@@ -15,22 +15,6 @@ type Props = $ReadOnly<{|
 |}>;
 
 export default function ModalNavBar(props: Props): Node {
-  // Layout from https://material.io/components/app-bars-top :
-  //  * When we do have a back button, we want 32px between icon and text,
-  //    and 16px before icon.  See handy spec diagrams at end of page.
-  //
-  //  * With no back button, we want 12px before start of text.  (This isn't
-  //    explicit in the guidelines, but see an example without back button
-  //    under "Cross-platform adaptation".  Zoom in and compare to the
-  //    neighboring example where there is one; this example has a bit less
-  //    padding than that one, which should have 16px.)
-  //
-  //  * At end of text, always put 12px, as we never have a button there.
-  //    (This isn't clear either, but use 12px for symmetry with start.)
-  //
-  // And the `NavBarBackButton` comes with 12px padding around icon and
-  // wants another 4px padding at start.
-
   const { canGoBack, title } = props;
   const { backgroundColor } = useContext(ThemeContext);
 
@@ -39,7 +23,7 @@ export default function ModalNavBar(props: Props): Node {
       text: [
         globalStyles.navTitle,
         { flex: 1 },
-        canGoBack ? { marginStart: 20, marginEnd: 8 } : { marginHorizontal: 8 },
+        canGoBack ? { marginStart: 8 } : { marginHorizontal: 8 }, // Reduced margin
       ],
       surface: {
         borderColor: 'hsla(0, 0%, 50%, 0.25)',
@@ -52,6 +36,9 @@ export default function ModalNavBar(props: Props): Node {
         alignItems: 'center',
         paddingHorizontal: 4,
       },
+      backButtonWrapper: {
+        width: canGoBack ? 48 : 0, // Ensure that space is available for the back button
+      },
     }),
     [canGoBack, backgroundColor],
   );
@@ -60,7 +47,9 @@ export default function ModalNavBar(props: Props): Node {
     <SafeAreaView mode="padding" edges={['top']} style={styles.surface}>
       <OfflineNoticePlaceholder />
       <SafeAreaView mode="padding" edges={['right', 'left']} style={styles.contentArea}>
-        {canGoBack && <NavBarBackButton />}
+        <View style={styles.backButtonWrapper}>
+          {canGoBack && <NavBarBackButton />}
+        </View>
         <ZulipTextIntl style={styles.text} text={title} numberOfLines={1} ellipsizeMode="tail" />
       </SafeAreaView>
     </SafeAreaView>

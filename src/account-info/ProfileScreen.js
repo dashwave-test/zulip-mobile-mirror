1cdf6765b41806abdf01130f04f77880c4c88d4a
Dashwave <tech@dashwave.io>
2024-10-25 07:52:39 +0000

    Refactor ProfileScreen to use NavRows and TextRow instead of ZulipButtons

--- src/account-info/ProfileScreen.js
+++ src/account-info/ProfileScreen.js
@@ -11,7 +11,6 @@ import type { RouteProp } from '../react-navigation';
 import type { MainTabsNavigationProp } from '../main/MainTabsScreen';
 import { createStyleSheet } from '../styles';
 import { useDispatch, useSelector } from '../react-redux';
-import ZulipButton from '../common/ZulipButton';
 import { logout } from '../account/logoutActions';
 import { tryStopNotifications } from '../notification/notifTokens';
 import AccountDetails from './AccountDetails';
@@ -23,9 +22,8 @@ import { showConfirmationDialog } from '../utils/info';
 import { OfflineNoticePlaceholder } from '../boot/OfflineNoticeProvider';
 import { getUserStatus } from '../user-statuses/userStatusesModel';
 import SwitchRow from '../common/SwitchRow';
-import * as api from '../api';
-import { identityOfAccount } from '../account/accountMisc';
 import NavRow from '../common/NavRow';
+import TextRow from '../common/TextRow';
 import { emojiTypeFromReactionType } from '../emoji/data';
 
 const styles = createStyleSheet({
@@ -38,77 +36,6 @@ const styles = createStyleSheet({
     margin: 8,
   },
 });
-
-function ProfileButton(props: {| +ownUserId: UserId |}) {
-  const navigation = useNavigation();
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Full profile"
-      onPress={() => {
-        navigation.push('account-details', { userId: props.ownUserId });
-      }}
-    />
-  );
-}
-
-function SettingsButton(props: {||}) {
-  const navigation = useNavigation();
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Settings"
-      onPress={() => {
-        navigation.push('settings');
-      }}
-    />
-  );
-}
-
-function SwitchAccountButton(props: {||}) {
-  const navigation = useNavigation();
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Switch account"
-      onPress={() => {
-        navigation.push('account-pick');
-      }}
-    />
-  );
-}
-
-function LogoutButton(props: {||}) {
-  const dispatch = useDispatch();
-  const _ = useContext(TranslationContext);
-  const account = useSelector(getAccount);
-  const identity = identityOfAccount(account);
-  return (
-    <ZulipButton
-      style={styles.button}
-      secondary
-      text="Log out"
-      onPress={() => {
-        showConfirmationDialog({
-          destructive: true,
-          title: 'Log out',
-          message: {
-            text: 'This will log out {email} on {realmUrl} from the mobile app on this device.',
-            values: { email: identity.email, realmUrl: identity.realm.toString() },
-          },
-          onPressConfirm: () => {
-            dispatch(tryStopNotifications(account));
-            dispatch(logout());
-          },
-          _,
-        });
-      }}
-    />
-  );
-}
 
 type Props = $ReadOnly<{|
   navigation: MainTabsNavigationProp<'profile'>,
@@ -130,6 +57,11 @@ export default function ProfileScreen(props: Props): Node {
   const userStatus = useSelector(state => getUserStatus(state, ownUserId));
 
   const { status_emoji, status_text } = userStatus;
+
+  const dispatch = useDispatch();
+  const _ = useContext(TranslationContext);
+  const account = useSelector(getAccount);
+  const identity = account && account.realm && account.email ? { realm: account.realm, email: account.email } : null;
 
   return (
     <SafeAreaView mode="padding" edges={['top']} style={{ flex: 1 }}>
@@ -172,17 +104,47 @@ export default function ProfileScreen(props: Props): Node {
             }}
           />
         )}
-        <View style={styles.buttonRow}>
-          <ProfileButton ownUserId={ownUser.user_id} />
-        </View>
-        <View style={styles.buttonRow}>
-          <SettingsButton />
-        </View>
-        <View style={styles.buttonRow}>
-          <SwitchAccountButton />
-          <LogoutButton />
-        </View>
+
+        <NavRow
+          title="Full profile"
+          onPress={() => {
+            navigation.push('account-details', { userId: ownUser.user_id });
+          }}
+        />
+        <NavRow
+          title="Settings"
+          onPress={() => {
+            navigation.push('settings');
+          }}
+        />
+        <NavRow
+          title="Switch account"
+          onPress={() => {
+            navigation.push('account-pick');
+          }}
+        />
+        {identity && (
+          <TextRow
+            title="Log out"
+            onPress={() => {
+              showConfirmationDialog({
+                destructive: true,
+                title: 'Log out',
+                message: {
+                  text: 'This will log out {email} on {realmUrl} from the mobile app on this device.',
+                  values: { email: identity.email, realmUrl: identity.realm.toString() },
+                },
+                onPressConfirm: () => {
+                  dispatch(tryStopNotifications(account));
+                  dispatch(logout());
+                },
+                _,
+              });
+            }}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
 }
+

b92b46be39395fcff219efa1a80aa0475b3438c7
Dashwave <tech@dashwave.io>
2024-10-25 07:47:58 +0000

    Fix StatusBar color inconsistency in landscape mode

--- src/common/ZulipStatusBar.js
+++ src/common/ZulipStatusBar.js
@@ -14,8 +14,12 @@ import { getThemeToUse } from '../settings/settingsSelectors';
 
 type BarStyle = React$ElementConfig<typeof StatusBar>['barStyle'];
 
-export const getStatusBarColor = (backgroundColor: string | void, theme: ThemeName): string =>
-  backgroundColor ?? (theme === 'dark' ? 'hsl(212, 28%, 18%)' : 'white');
+export const getStatusBarColor = (backgroundColor: string | void, theme: ThemeName, orientation: string): string => {
+  if (orientation === 'LANDSCAPE') {
+    return theme === 'dark' ? 'hsl(212, 28%, 25%)' : 'lightgray';
+  }
+  return backgroundColor ?? (theme === 'dark' ? 'hsl(212, 28%, 18%)' : 'white');
+};
 
 export const getStatusBarStyle = (statusBarColor: string): BarStyle =>
   foregroundColorFromBackground(statusBarColor) === 'white' /* force newline */
@@ -52,17 +56,15 @@ export default function ZulipStatusBar(props: Props): Node {
 
   const orientation = useGlobalSelector(state => getGlobalSession(state).orientation);
   const backgroundColor = props.backgroundColor;
-  const statusBarColor = getStatusBarColor(backgroundColor, themeToUse);
+  const statusBarColor = getStatusBarColor(backgroundColor, themeToUse, orientation);
 
   return (
-    orientation === 'PORTRAIT' && (
-      <StatusBar
-        animated
-        showHideTransition="slide"
-        hidden={hidden && Platform.OS !== 'android'}
-        backgroundColor={Color(statusBarColor).darken(0.1).hsl().string()}
-        barStyle={getStatusBarStyle(statusBarColor)}
-      />
-    )
+    <StatusBar
+      animated
+      showHideTransition="slide"
+      hidden={hidden && Platform.OS !== 'android'}
+      backgroundColor={Color(statusBarColor).darken(0.1).hsl().string()}
+      barStyle={getStatusBarStyle(statusBarColor)}
+    />
   );
 }

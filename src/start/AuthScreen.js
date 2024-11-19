cd1fb4b56e035993e04491f8c1b3977e70a3ccf5
Dashwave <tech@dashwave.io>
2024-11-19 10:59:39 +0000

    Add info button on login page for Zulip docs

--- src/start/AuthScreen.js
+++ src/start/AuthScreen.js
@@ -2,7 +2,7 @@
 
 import React, { PureComponent } from 'react';
 import type { ComponentType } from 'react';
-import { Linking, Platform } from 'react-native';
+import { Linking, Platform, View } from 'react-native';
 import type { AppleAuthenticationCredential } from 'expo-apple-authentication';
 import * as AppleAuthentication from 'expo-apple-authentication';
 
@@ -22,6 +22,7 @@ import {
   IconGitHub,
   IconWindows,
   IconTerminal,
+  IconInfo,
 } from '../common/Icons';
 import type { SpecificIconType } from '../common/Icons';
 import { connectGlobal } from '../react-redux';
@@ -43,7 +44,7 @@ import { openLinkEmbedded } from '../utils/openLink';
  * described in the /server_settings response; see api.getServerSettings
  * and https://zulip.com/api/get-server-settings .
  */
-type AuthenticationMethodDetails = {|
+type AuthenticationMethodDetails = {|  
   /** An identifier-style name used in the /server_settings API. */
   name: string,
 
@@ -142,7 +143,7 @@ type OuterProps = $ReadOnly<{|
   navigation: AppNavigationProp<'auth'>,
   route: RouteProp<
     'auth',
-    {|
+    {|      
       // Keep constant through the life of an 'auth' route: don't
       // `navigation.navigate` or `navigation.setParams` or do anything else
       // that can change this. We use it to identify the server to the user,
@@ -303,6 +304,11 @@ class AuthScreenInner extends PureComponent<Props> {
     }
   };
 
+  handleInfoPress = () => {
+    const documentationUrl = 'https://zulip.com/help/logging-in';
+    Linking.openURL(documentationUrl);
+  };
+
   render() {
     const { serverSettings } = this.props.route.params;
 
@@ -337,6 +343,14 @@ class AuthScreenInner extends PureComponent<Props> {
               />
             ),
           )}
+          <View style={styles.halfMarginTop}>
+            <ZulipButton
+              secondary
+              text="Info"
+              Icon={IconInfo}
+              onPress={this.handleInfoPress}
+            />
+          </View>
         </Centerer>
       </Screen>
     );
